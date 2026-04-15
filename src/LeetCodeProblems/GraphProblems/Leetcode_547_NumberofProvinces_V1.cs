2eabb740a08c687e26d45d47388339b3a5f66dff
Koppineni, Pavan kumar <p.koppineni@spglobal.com>
2026-04-15 16:35:57 +0530

    added new problem

---
+++ src/LeetCodeProblems/GraphProblems/Leetcode_547_NumberofProvinces_V1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.GraphProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/number-of-provinces/description/?envType=problem-list-v2&envId=graph
+    /// </summary>
+    public class Leetcode_547_NumberofProvinces_V1
+    {
+        public int CalculateNumberOfProvinces(int[][] isConnected)
+        {
+            var numberOfProvinces = 0;
+            var visited = new HashSet<int>();
+            var numberOfCities = isConnected.Length;
+            for (var index = 0; index < numberOfCities; index++)
+            {
+                if (visited.Contains(index))
+                {
+                    continue;
+                }
+
+                numberOfProvinces += 1;
+                DFS(visited, index, isConnected);
+            }
+
+            return numberOfProvinces;
+        }
+
+        private void DFS(HashSet<int> visited, int city, int[][] m)
+        {
+            visited.Add(city);
+            for (var index = 0; index < m.Length; index++)
+            {
+                if (visited.Contains(index) || m[city][index] == 0)
+                {
+                    continue;
+                }
+
+                DFS(visited, index, m);
+            }
+        }
+    }
+}
---
+++ src/LeetCodeProblems/GraphProblems/Leetcode_743_NetworkDelayTime_V1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.GraphProblems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/network-delay-time/description/?envType=problem-list-v2&envId=graph
+    /// </summary>
+    public class Leetcode_743_NetworkDelayTime_V1
+    {
+        public class GraphNode
+        {
+            public int Node { get; set; }
+            public int Time { get; set; }
+        }
+
+        public int CalculateNetworkDelayTime(int[][] times, int n, int k)
+        {
+            var minTime = GetMinTime(n, k);
+            var graph = BuildGraph(times);
+            return CalculateNetworkDelayTimeInternal(graph, minTime, k);
+        }
+
+        private int CalculateNetworkDelayTimeInternal(IDictionary<int, IList<GraphNode>> graph, int[] minTime, int startNode)
+        {
+            var visited = new HashSet<int>();
+            var queue = new Queue<int>();
+            queue.Enqueue(startNode);
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                visited.Add(node);
+                var edges = graph.ContainsKey(node) ? graph[node] : new List<GraphNode>();
+                if (edges.Count == 0)
+                {
+                    continue;
+                }
+
+                foreach (var edge in edges)
+                {
+                    var edgeNode = edge.Node;
+                    var networkTime = edge.Time + minTime[node - 1];
+                    if (networkTime < minTime[edgeNode - 1])
+                    {
+                        minTime[edgeNode - 1] = networkTime;
+                        queue.Enqueue(edgeNode);
+                    }
+                }
+            }
+
+            if (visited.Count != minTime.Length)
+            {
+                return -1;
+            }
+
+            return minTime.Max();
+        }
+
+        private IDictionary<int, IList<GraphNode>> BuildGraph(int[][] times)
+        {
+            var graph = new Dictionary<int, IList<GraphNode>>();
+            for (var index = 0; index < times.Length; index++)
+            {
+                var time = times[index];
+                var from = time[0];
+                var to = time[1];
+                if (!graph.ContainsKey(from))
+                {
+                    graph.Add(from, new List<GraphNode>());
+                }
+                graph[from].Add(new GraphNode { Node = to, Time = time[2] });
+            }
+            return graph;
+        }
+
+        private int[] GetMinTime(int n, int k)
+        {
+            var minTime = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                minTime[i] = int.MaxValue;
+            }
+            minTime[k - 1] = 0;
+
+            return minTime;
+        }
+    }
+}
